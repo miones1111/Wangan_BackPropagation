cb1ef8b5ae1abcf4337136080f4f3528c9afc8ff
john william miones <111831441+miones1111@users.noreply.github.com>
2023-03-11 12:14:45 +0800

    Create Forms and add datasets

---
+++ TeamWangan_BackPropagation/TeamWangan_BackPropagation/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace TeamWangan_BackPropagation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtInput1 = new System.Windows.Forms.TextBox();
+            this.txtInput2 = new System.Windows.Forms.TextBox();
+            this.txtInput3 = new System.Windows.Forms.TextBox();
+            this.txtInput4 = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnTrain = new System.Windows.Forms.Button();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTraining = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(145, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Back Propagation";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "HEMOGLOBIN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(113, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "MCH";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(99, 238);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 22);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "MCHC";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(106, 313);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 22);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "MCV";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(69, 383);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 22);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "RESULT";
+            // 
+            // txtInput1
+            // 
+            this.txtInput1.Location = new System.Drawing.Point(201, 124);
+            this.txtInput1.Name = "txtInput1";
+            this.txtInput1.Size = new System.Drawing.Size(206, 22);
+            this.txtInput1.TabIndex = 7;
+            // 
+            // txtInput2
+            // 
+            this.txtInput2.Location = new System.Drawing.Point(201, 179);
+            this.txtInput2.Name = "txtInput2";
+            this.txtInput2.Size = new System.Drawing.Size(206, 22);
+            this.txtInput2.TabIndex = 8;
+            // 
+            // txtInput3
+            // 
+            this.txtInput3.Location = new System.Drawing.Point(201, 240);
+            this.txtInput3.Name = "txtInput3";
+            this.txtInput3.Size = new System.Drawing.Size(206, 22);
+            this.txtInput3.TabIndex = 9;
+            // 
+            // txtInput4
+            // 
+            this.txtInput4.Location = new System.Drawing.Point(201, 315);
+            this.txtInput4.Name = "txtInput4";
+            this.txtInput4.Size = new System.Drawing.Size(206, 22);
+            this.txtInput4.TabIndex = 10;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Enabled = false;
+            this.txtResult.Location = new System.Drawing.Point(201, 385);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(206, 22);
+            this.txtResult.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(26, 460);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 47);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnTrain
+            // 
+            this.btnTrain.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTrain.Location = new System.Drawing.Point(171, 460);
+            this.btnTrain.Name = "btnTrain";
+            this.btnTrain.Size = new System.Drawing.Size(101, 47);
+            this.btnTrain.TabIndex = 13;
+            this.btnTrain.Text = "Epoch";
+            this.btnTrain.UseVisualStyleBackColor = true;
+            this.btnTrain.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnTest
+            // 
+            this.btnTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTest.Location = new System.Drawing.Point(315, 460);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(101, 47);
+            this.btnTest.TabIndex = 14;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 546);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 22);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "TRAINING TIMES";
+            // 
+            // txtTraining
+            // 
+            this.txtTraining.Location = new System.Drawing.Point(210, 548);
+            this.txtTraining.Name = "txtTraining";
+            this.txtTraining.Size = new System.Drawing.Size(206, 22);
+            this.txtTraining.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 615);
+            this.Controls.Add(this.txtTraining);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.btnTrain);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtInput4);
+            this.Controls.Add(this.txtInput3);
+            this.Controls.Add(this.txtInput2);
+            this.Controls.Add(this.txtInput1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtInput1;
+        private System.Windows.Forms.TextBox txtInput2;
+        private System.Windows.Forms.TextBox txtInput3;
+        private System.Windows.Forms.TextBox txtInput4;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnTrain;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTraining;
+    }
+}
+
---
+++ TeamWangan_BackPropagation/TeamWangan_BackPropagation/Form1.cs
@@ -0,0 +1,261 @@
+﻿using Backprop;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeamWangan_BackPropagation
+{
+    public partial class Form1 : Form
+    {
+        NeuralNet nn;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nn = new NeuralNet(1, 10, 1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //initiate inputs, desired outputs and let the algorithm learn
+            for (int i = 0; i < Convert.ToInt32(txtTraining.Text); i++)
+            {
+                nn.setInputs(0, 14.9);
+                nn.setInputs(1, 22.7);
+                nn.setInputs(2, 29.1);
+                nn.setInputs(3, 83.7);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 15.9);
+                nn.setInputs(1, 25.4);
+                nn.setInputs(2, 28.3);
+                nn.setInputs(3, 72.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 9.0);
+                nn.setInputs(1, 21.5);
+                nn.setInputs(2, 29.6);
+                nn.setInputs(3, 71.2);
+                nn.setDesiredOutput(0, 1.0);
+                nn.learn();
+                
+                nn.setInputs(0, 14.9);
+                nn.setInputs(1, 16.0);
+                nn.setInputs(2, 31.4);
+                nn.setInputs(3, 87.5);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 14.7);
+                nn.setInputs(1, 22.0);
+                nn.setInputs(2, 28.2);
+                nn.setInputs(3, 99.5);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 11.6);
+                nn.setInputs(1, 22.3); 
+                nn.setInputs(2, 30.9);
+                nn.setInputs(3, 74.5);
+                nn.setDesiredOutput(0, 1.0);
+                nn.learn();
+
+                nn.setInputs(0, 12.7);
+                nn.setInputs(1, 19.5);
+                nn.setInputs(2, 28.9);
+                nn.setInputs(3, 82.1);
+                nn.setDesiredOutput(0, 1.0);
+                nn.learn();
+
+                nn.setInputs(0, 12.7);
+                nn.setInputs(1, 28.5);
+                nn.setInputs(2, 28.2);
+                nn.setInputs(3, 92.3);
+                nn.setDesiredOutput(0, 1.0);
+                nn.learn();
+
+                nn.setInputs(0, 14.1);
+                nn.setInputs(1, 29.7);
+                nn.setInputs(2, 30.5); 
+                nn.setInputs(3, 75.2);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 14.9);
+                nn.setInputs(1, 25.8);
+                nn.setInputs(2, 31.3);	
+                nn.setInputs(3, 82.9);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 13.0);
+                nn.setInputs(1, 18.3);
+                nn.setInputs(2, 29.6); 
+                nn.setInputs(3, 87.8);
+                nn.setDesiredOutput(0, 1.0);
+                nn.learn();
+
+                nn.setInputs(0, 16.7);
+                nn.setInputs(1, 27.5);
+                nn.setInputs(2, 28.2);
+                nn.setInputs(3, 93.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 13.4);
+                nn.setInputs(1, 25.2);
+                nn.setInputs(2, 30.2);
+                nn.setInputs(3, 95.9);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 14.7);
+                nn.setInputs(1, 28.9);
+                nn.setInputs(2, 31.0);
+                nn.setInputs(3, 69.8);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 15.9);
+                nn.setInputs(1, 24.3);
+                nn.setInputs(2, 28.7);
+                nn.setInputs(3, 91.5);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 14.0);
+                nn.setInputs(1, 25.5);
+                nn.setInputs(2, 32.0);
+                nn.setInputs(3, 81.6);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 15.9);
+                nn.setInputs(1, 24.0);
+                nn.setInputs(2, 27.9);
+                nn.setInputs(3, 83.7);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 12.3);
+                nn.setInputs(1, 21.8);
+                nn.setInputs(2, 27.8);
+                nn.setInputs(3, 77.9);
+                nn.setDesiredOutput(0, 1.0);
+                nn.learn();
+
+                nn.setInputs(0, 15.4);
+                nn.setInputs(1, 24.6);
+                nn.setInputs(2, 30.1);
+                nn.setInputs(3, 94.3);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 13.6);
+                nn.setInputs(1, 19.3);
+                nn.setInputs(2, 31.4);
+                nn.setInputs(3, 78.9);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 16.5);
+                nn.setInputs(1, 19.3);
+                nn.setInputs(2, 29.2);
+                nn.setInputs(3, 69.7);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 16.2);
+                nn.setInputs(1, 17.2);
+                nn.setInputs(2, 32.2);
+                nn.setInputs(3, 78.4);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 6.9);
+                nn.setInputs(1, 28.1);
+                nn.setInputs(2, 32.5);
+                nn.setInputs(3, 94.6);
+                nn.setDesiredOutput(0, 1.0);
+                nn.learn();
+
+                nn.setInputs(0, 11.8);
+                nn.setInputs(1, 16.3);
+                nn.setInputs(2, 30.9);
+                nn.setInputs(3, 78.7);
+                nn.setDesiredOutput(0, 1.0);
+                nn.learn();
+
+                nn.setInputs(0, 11.0);
+                nn.setInputs(1, 26.0);
+                nn.setInputs(2, 32.2);
+                nn.setInputs(3, 98.9);
+                nn.setDesiredOutput(0, 1.0);
+                nn.learn();
+
+                nn.setInputs(0, 11.0);
+                nn.setInputs(1, 25.2);	
+                nn.setInputs(2, 30.9);
+                nn.setInputs(3, 83.2);
+                nn.setDesiredOutput(0, 1.0);
+                nn.learn();
+
+                nn.setInputs(0, 10.7);
+                nn.setInputs(1, 21.3); 	
+                nn.setInputs(2, 29.1);
+                nn.setInputs(3, 78.7);
+                nn.setDesiredOutput(0, 1.0);
+                nn.learn();
+
+                nn.setInputs(0, 14.8);
+                nn.setInputs(1, 23.4);
+                nn.setInputs(2, 29.2);
+                nn.setInputs(3, 74.7);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 12.1);
+                nn.setInputs(1, 28.3);
+                nn.setInputs(2, 29.2);
+                nn.setInputs(3, 99.7);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 14.7);
+                nn.setInputs(1, 23.4);
+                nn.setInputs(2, 29.9);
+                nn.setInputs(3, 85.3);
+                nn.setDesiredOutput(0, 0);
+                nn.learn();
+            }
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            nn.setInputs(0, Convert.ToDouble(txtInput1.Text));
+            nn.setInputs(1, Convert.ToDouble(txtInput2.Text));
+            nn.setInputs(2, Convert.ToDouble(txtInput3.Text));
+            nn.setInputs(3, Convert.ToDouble(txtInput4.Text));
+            nn.run();
+            txtResult.Text = "" + nn.getOuputData(0);
+
+        }
+
+        
+    }
+}
